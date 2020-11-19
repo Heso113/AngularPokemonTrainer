286ea8f9aa6fdc68ee8289818618f26d34656f80
NORDIC\pkarim <paria.karim@gmail.com>
2020-11-19 14:33:52 +0100

    lillap feat: added button at trainer-page with route to pokemon catalog

--- src/app/components/pages/trainer-page/trainer-page.component.ts
+++ src/app/components/pages/trainer-page/trainer-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionService } from 'src/app/services/session/session.service';
 
 
 @Component({
@@ -8,14 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainerPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private session: SessionService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  
 
- 
- 
+  async onGetPokemonCatalogue() {
+    if (this.session.get() !== '') {
+      this.router.navigateByUrl('/pokemonCatalogue');
+    } else {
+      this.router.navigateByUrl('/loginForm');
+    }
+  }
 
 }
