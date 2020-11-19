c6df607eacead41efd50edf2e8d3086ef8877cfb
Heso113 <hampe_olsson91@hotmail.com>
2020-11-19 13:46:05 +0100

    fix: store pokemon information locally for future use.

--- src/app/components/pages/trainer-page/trainer-page.component.ts
+++ src/app/components/pages/trainer-page/trainer-page.component.ts
@@ -9,8 +9,10 @@ import { PokemonAPIService } from 'src/app/services/pokemonAPI/pokemon-api.servi
 export class TrainerPageComponent implements OnInit {
 
   currentPokemonPage = [];
+  
 
-  constructor(private api: PokemonAPIService) { }
+  constructor(private api: PokemonAPIService) {
+   }
 
   ngOnInit(): void {
   }
@@ -21,21 +23,22 @@ export class TrainerPageComponent implements OnInit {
     console.log(pokemons);
   }
 
-  async getPageOfPokemons(index: number) {
-    let page = await this.api.getPokemonPage(index);
-    // this.currentPokemonPage = page.results;
-    // console.log(this.currentPokemonPage);
-    // console.log(page);
-    this.currentPokemonPage = [];
-    for (let i = 0; i < 50; i++) {
-      let pokemon = {name: '', imgUrl: ''};
-      pokemon.name = page.results[i].name;
-      let url = page.results[i].url;
-      let pokemonDetails = await this.api.getPokemonByUrl(url);
-      pokemon.imgUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + pokemonDetails.id + '.png';
-      console.log(pokemonDetails);
-      this.currentPokemonPage.push(pokemon);
-    }
+  getPageOfPokemons(index: number) {
+    // let page = await this.api.getPokemonPage(index);
+    // // this.currentPokemonPage = page.results;
+    // // console.log(this.currentPokemonPage);
+    // // console.log(page);
+    // this.currentPokemonPage = [];
+    // for (let i = 0; i < 50; i++) {
+    //   let pokemon = {name: '', imgUrl: ''};
+    //   pokemon.name = page.results[i].name;
+    //   let url = page.results[i].url;
+    //   let pokemonDetails = await this.api.getPokemonByUrl(url);
+    //   pokemon.imgUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + pokemonDetails.id + '.png';
+    //   console.log(pokemonDetails);
+    //   this.currentPokemonPage.push(pokemon);
+    // }
+    this.currentPokemonPage = this.api.getPreLoadedPokemonPage(index);
   }
 
   async getPokemonByUrl(url: string) {
--- src/app/services/pokemonAPI/pokemon-api.service.ts
+++ src/app/services/pokemonAPI/pokemon-api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { stringify } from 'querystring';
 
 @Injectable({
   providedIn: 'root'
@@ -7,22 +8,53 @@ import { Injectable } from '@angular/core';
 export class PokemonAPIService {
 
   url = 'https://pokeapi.co/api/v2/';
-  nrOfPokemonCardsPerPage = 50;
+  pokemonCount = 0;
+  numberOfPages = 0;
+  nrOfPokemonsPerPage = 50;
+  pokemonPages = [];
 
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.initTrainerPage();
+  }
 
+  async initTrainerPage() {
+    let pokemonInfo = await this.getAllPokemons();
+    this.pokemonCount = pokemonInfo.count;
+    this.numberOfPages = this.pokemonCount / this.nrOfPokemonsPerPage;
+    console.log(this.pokemonCount);
+    console.log(this.numberOfPages);
+    console.log(pokemonInfo);
+    for (let i = 0; i < this.numberOfPages; i++) {
+      let nextSetOfPokemons = await this.getPokemonPage(i);
+      console.log(nextSetOfPokemons);
+      let nextPage = [];
+      for (let p = 0; p < this.nrOfPokemonsPerPage; p++) {
+        let nextPokemon = await this.getPokemonByUrl(nextSetOfPokemons.results[p].url);
+        let name = nextSetOfPokemons.results[p].name;
+        let imgUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + nextPokemon.id + '.png';
+        let pokemonObject = {name, imgUrl};
+        nextPage.push(pokemonObject);
+      }
+      this.pokemonPages.push(nextPage);
+    }
+    console.log(this.pokemonPages);
+  }
   
   getAllPokemons() {
     return this.http.get(this.url + 'pokemon?limit=1050&offset=0').toPromise();
   }
 
   getPokemonPage(pageIndex: number) {
-    const offset = pageIndex * this.nrOfPokemonCardsPerPage;
-    return this.http.get(this.url + 'pokemon?limit=' + this.nrOfPokemonCardsPerPage + '&offset=' + offset).toPromise();
+    const offset = pageIndex * this.nrOfPokemonsPerPage;
+    return this.http.get(this.url + 'pokemon?limit=' + this.nrOfPokemonsPerPage + '&offset=' + offset).toPromise();
   }
 
   getPokemonByUrl(url: string) {
     return this.http.get(url).toPromise();
+  }
+
+  getPreLoadedPokemonPage(pageIndex: number) {
+    return this.pokemonPages[pageIndex];
   }
 }
