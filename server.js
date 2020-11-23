05b21871a3ad8b68c111dff9e22b340807726a06
NORDIC\pkarim <paria.karim@gmail.com>
2020-11-23 09:44:32 +0100

    lillap refactor: changed path in server.js for deployment

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-pokemon-trainer'));
+app.use(express.static(__dirname + '/dist/AngularPokemonTrainer'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angular-pokemon-trainer/index.html'));
+res.sendFile(path.join(__dirname+'/dist/AngularPokemonTrainer/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
